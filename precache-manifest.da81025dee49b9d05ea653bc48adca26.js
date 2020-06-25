1598a5ffb4acf75bfe233981d4611fcf5cfb8802
yejin <johnyejin@kakao.com>
2020-06-26 05:02:30 +0900

    Updates

---
+++ precache-manifest.da81025dee49b9d05ea653bc48adca26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5591ef034e5cf10cefe22b70fccbf8f5",
+    "url": "/news-viewer/index.html"
+  },
+  {
+    "revision": "c0e922272ef8056a1e81",
+    "url": "/news-viewer/static/js/2.2f4dc9fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-viewer/static/js/2.2f4dc9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79865baecdc07117b2f",
+    "url": "/news-viewer/static/js/main.b5ccbff3.chunk.js"
+  },
+  {
+    "revision": "ef96a0caf70099cc53ce",
+    "url": "/news-viewer/static/js/runtime-main.cdcd38ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/johnyejin.github-io.news-viewer/precache-manifest.ceec8220ac7c9330b2f0c29d46a049cc.js"
+  "/news-viewer/precache-manifest.da81025dee49b9d05ea653bc48adca26.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/johnyejin.github-io.news-viewer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-viewer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
